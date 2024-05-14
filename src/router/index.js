b973eb235752067825d218fa87bec89ec451cb7c
0xPrakharG <prakhargoyal.3014@gmail.com>
2024-05-14 20:37:06 +0530

    Complete official site frontend

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,5 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import HomeView from '@/views/HomeView.vue'
 import VerificationView from '@/views/VerificationView.vue'
 import HelpView from '@/views/HelpView.vue'
 import LoginView from '@/views/LoginView.vue'
@@ -17,7 +17,7 @@ const router = createRouter({
     },
     {
       path: '/verification',
-      name: 'verification',
+      name: 'Verification',
       component: VerificationView
     },
     {
@@ -32,7 +32,7 @@ const router = createRouter({
     },
     {
       path: '/signup',
-      name: 'SignUp',
+      name: 'Signup',
       component: SignupView
     },
     {
